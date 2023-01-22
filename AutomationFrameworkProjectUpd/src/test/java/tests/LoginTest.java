84ca1201ed2462a03fe8d234c09f5140169f7833
h <h@hp>
2023-01-22 16:33:02 +0530

    22/01/23 commit

--- AutomationFrameworkProjectUpd/src/test/java/tests/LoginTest.java
+++ AutomationFrameworkProjectUpd/src/test/java/tests/LoginTest.java
@@ -1,10 +1,15 @@
 package tests;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
 
 import pageobject.HomePage;
 import pageobject.LandingPage;
@@ -14,14 +19,18 @@ import resources.Base;
 public class LoginTest extends Base{
 
 	WebDriver driver;
+	Logger log;
 	
-	@Test
-	public void loginTest() throws Exception
+	@Test(dataProvider ="LoginData" )
+	public void loginTest(String email,String pwd) throws Exception
 	{
 		LandingPage landingpage=new LandingPage(driver);
 		LoginPage loginpage=landingpage.clickLoginBtn();
-		HomePage homepage=loginpage.loinToApplication(prop.getProperty("email"), prop.getProperty("password"));
+		log.info("Clicking Login Button..");
+		//HomePage homepage=loginpage.loinToApplication(prop.getProperty("email"), prop.getProperty("password"));
+		HomePage homepage=loginpage.loinToApplication(email, pwd);
 		Assert.assertTrue(homepage.loginSuccess(), "Failed to login to application");
+		log.info("Successfully Log in to application..");
 		homepage.logoutFromApplication();
 	}
 	
@@ -29,12 +38,22 @@ public class LoginTest extends Base{
 	public void tearDown()
 	{
 		driver.quit();
+		log.info("All Browsers opened by automation closed sucessfully..");
 	}
 	
 	@BeforeTest
 	public void initialiseDriver() throws Exception
 	{
+		log = LogManager.getLogger(LoginTest.class.getName());
 		driver=driverInitializer();
+		log.info("Driver initialised successfully");
 		driver.get(prop.getProperty("url"));
+		log.debug("URL launch successfully...");
+	}
+	
+	@DataProvider(name = "LoginData")
+	public Object[][] getLoginData()
+	{
+		return new Object[][]{{"testpm@test.com","test"},{"dummy@dummy.com","dummy"}};
 	}
 }
--- AutomationFrameworkProjectUpd/src/test/java/tests/RegistrationTest.java
+++ AutomationFrameworkProjectUpd/src/test/java/tests/RegistrationTest.java
@@ -1,8 +1,12 @@
 package tests;
 
 import org.openqa.selenium.WebDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageobject.LandingPage;
@@ -13,26 +17,38 @@ import resources.Base;
 public class RegistrationTest extends Base{
 	
 	WebDriver driver;
+	Logger log;
 	
-	@Test
-	public void RegistrationTest()
+	@Test(dataProvider ="RegistrationData" )
+	public void doRegistrationTest(String firstName, String lastName,String email, String phoneNo,String pwd)
 	{
 		LandingPage landingpage=new LandingPage(driver);
 		RegistrationPage registrationpage=landingpage.clickRegistrationBtn();
-		registrationpage.doRegistration("testname", "testLastName", "emailInput@test.com", "112233", "test");
+		log.info("Registration done successfully..");
+		registrationpage.doRegistration(firstName,lastName,email,phoneNo,pwd);
 	}
 	
 	@AfterTest
 	public void tearDown()
 	{
 		driver.quit();
+		log.info("All browsers opened by automation got closed successfully...");
 	}
 	
 	@BeforeTest
 	public void initialiseDriver() throws Exception
 	{
+		log=LogManager.getLogger(RegistrationTest.class.getName());
 		driver=driverInitializer();
+		log.debug("driver got initialised successfully..");
 		driver.get(prop.getProperty("url"));
+		log.debug("URL got launched successfully...");
+	}
+	
+	@DataProvider(name="RegistrationData")
+	public Object[][] getRegistrationData()
+	{
+		return new Object[][] {{"testname", "testLastName", "emailInput@test.com", "112233", "test"}};
 	}
 	
 	
